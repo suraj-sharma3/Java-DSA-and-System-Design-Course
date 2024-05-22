d572f26b5323cbf7bfd43b9216fe30e15c9aa507
suraj-sharma3 <suraj.shara13395@gmail.com>
2024-05-22 22:49:26 +0530

    Switch Case Tutorial Added

---
+++ Operators_and_Loops_Module_16/SwitchCase.java
@@ -0,0 +1,46 @@
+public class SwitchCase {
+    public static void main(String[] args) {
+        int num = 29;
+
+        /* switch(num){
+            case 100:
+                System.out.println("Case 1");
+            case 200:
+                System.out.println("Case 2");
+            case 29:
+                System.out.println("Case 3");
+            case 300:
+                System.out.println("Case 4");
+        } */
+
+        /* switch(num){
+            case 37:
+                System.out.println("Case 1");
+                break;
+            case 29:
+                System.out.println("Case 2");
+                break;
+            case 333:
+                System.out.println("Case 3");
+                break;
+            default:
+                System.out.println("Default Case");
+            
+        } */
+
+        switch(66 + 34){
+            case 150:
+                System.out.println("Case 1");
+                break;
+            case 250:
+                System.out.println("Case 2");
+                break;
+            case 350:
+                System.out.println("Case 3");
+                break;
+            default:
+                System.out.println("Default Case");
+            
+        }
+    }
+}
